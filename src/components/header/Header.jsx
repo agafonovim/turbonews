a897ff0f6ac7dd43a20e2d762235c6aa57d66dd1
agafonovim <andrey@agafonov.im>
2020-09-16 05:12:59 +0300

    header maded

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,11 @@ import './Header.scss';
 const Header = () => (
     <header className="header">
         <div className="user-panel">
-
+            <ul className="user-panel__list">
+                <li className="user-panel__list-item">100.00 ₽</li>
+                <li className="user-panel__list-item">username</li>
+                <li className="user-panel__list-item">Выйти</li>
+            </ul>
         </div>
     </header>
 );
